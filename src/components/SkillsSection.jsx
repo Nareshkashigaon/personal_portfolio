7b9fa65e25d5ec6eb89408536a82e7d44508878b
Nareshkashigaon <nareshkashigaon@gmail.com>
2025-07-30 17:42:05 +0530

    improved responsiveness

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -54,7 +54,7 @@ export const SkillsSection = () => {
           ))}
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2  sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredSkills.map((skill, key) => (
             <div
               key={key}
